103e025b1fba996cfb4b8b4fd0fc2d5669e63f92
Scarla-rgb <scarlarodriguez0620@gmail.com>
2021-04-02 18:03:38 -0400

    Crea funcion saveTask

--- src/node-CLI/tools/files.js
+++ src/node-CLI/tools/files.js
@@ -24,3 +24,23 @@ const loadtask = () => {
     }
     return listalk;
 }
+
+//Funcion para guardar los task en task.json
+const saveTask = (listTask) => {
+
+    let data = JSON.stringify(listTask);
+
+
+    fs.writeFile(filepath, data, (error) => {
+        if (error) {
+            throw new Error("Error create by me: --", error);
+
+        }
+    })
+
+}
+
+module.exports = {
+    loadtask,
+    saveTask
+}
